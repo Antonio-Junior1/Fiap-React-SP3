6cd05eae7c86c616dc9ecfdc5f51566c97308770
Antonio-Junior1 <162357815+Antonio-Junior1@users.noreply.github.com>
2024-09-16 02:35:30 -0300

    adicao componente

--- Fiap-project/src/pages/Login/index.tsx
+++ Fiap-project/src/pages/Login/index.tsx
@@ -2,6 +2,7 @@ import { HeaderBasic } from "../../components/HeaderBasic";
 import { Container } from "../../styles";
 import { MainContentLogin } from "../../components/MainContentLogin";
 import FooterLogin from "../../components/FooterExtend";
+import DevelopersSection from "../../components/Desenvolvedores/Index";
 
 
 const Login = () => {
@@ -9,6 +10,7 @@ const Login = () => {
         <Container>      
             <HeaderBasic/>
             <MainContentLogin/>
+            <DevelopersSection />
             <FooterLogin/>
         </Container>
     ) 
