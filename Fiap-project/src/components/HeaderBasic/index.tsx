6b28e6587d4bf49cd27c8c08e9a70acc69a5d9b5
Carlos Campos <caduferrazcampos04@gmail.com>
2024-09-16 13:01:32 -0300

    Cd
    Update2 index.tsx

--- Fiap-project/src/components/HeaderBasic/index.tsx
+++ Fiap-project/src/components/HeaderBasic/index.tsx
@@ -2,4 +2,12 @@ import { StyledHeader, StyledImage } from "./styles";
 import Portologo from "../../assets/PortoLogo.png"
 
 export function HeaderBasic(){
-}
+
+    return(
+        <StyledHeader>
+          <a href="/"> < StyledImage src= {Portologo} />    </a>         
+        </StyledHeader>
+)
+
+
+} 
