59400f19964a28bba504670eda880ba7a39d72ef
Antonio-Junior1 <162357815+Antonio-Junior1@users.noreply.github.com>
2024-09-16 00:40:49 -0300

    s

--- Fiap-project/src/main.tsx
+++ Fiap-project/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {App} from './App.tsx'
+import './styles.ts'
+import {GlobalStyle} from './GlobalStyle.ts'
+
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <GlobalStyle/>
+    <App />    
   </StrictMode>,
 )
